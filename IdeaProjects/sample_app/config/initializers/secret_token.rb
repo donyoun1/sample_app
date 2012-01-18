ad5819d23655c4998eb94300749332fa19f37498
donyoun <dhyoun@teliax.com>
2012-01-17 17:18:29 -0700

    Initial commit

---
+++ IdeaProjects/sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'a54f052a90408fdf773e36dbbce5e142584f3248ebec2d57845a01f670310baccc3336c61c8a40cd990a78ab3b0dac9b4d0f7e1898e5e5e63381560bb9160a77'
